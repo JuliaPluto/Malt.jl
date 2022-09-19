61e248dfeaec5f93ca06df98921af1a2636ef11b
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2022-09-19 15:46:06 -0500

    Add tests for worker thrown exceptions

--- test/runtests.jl
+++ test/runtests.jl
@@ -78,3 +78,69 @@ end
     @test m.isrunning(w) === false
 end
 
+@testset "Exceptions" begin
+    w = m.Worker()
+
+
+    ## Mutually Known errors are not thrown, but returned as values.
+
+    result = m.remote_eval_fetch(w, quote
+        sqrt(-1)
+    end)
+    @test result isa DomainError
+
+    result2 = m.remote_eval_fetch(w, quote
+        error("Julia stack traces are bad. GL 😉")
+    end)
+    @test result2 isa ErrorException
+
+
+    ## Serializing values of unknown types will cause an exception.
+
+    stub_type_name = gensym(:NonLocalType)
+
+    m.remote_eval_wait(w, quote
+        struct $(stub_type_name) end
+    end)
+
+    @test_throws(TaskFailedException,
+        m.remote_eval_fetch(w, quote
+            $stub_type_name()
+        end),
+    )
+
+
+    ## Throwing unknown exceptions will definitely cause an exception.
+
+    stub_type_name2 = gensym(:NonLocalException)
+
+    m.remote_eval_wait(w, quote
+        struct $stub_type_name2 <: Exception end
+    end)
+
+    @test_throws(TaskFailedException,
+        m.remote_eval_fetch(w, quote
+            throw($stub_type_name2())
+        end),
+    )
+
+
+    ## Catching unknown exceptions and returning them as values also causes an exception.
+    ## (just like any other unknown type).
+
+    @test_throws(TaskFailedException,
+        m.remote_eval_fetch(w, quote
+            try
+                throw($stub_type_name2())
+            catch e
+                e
+            end
+        end),
+    )
+
+
+    # The worker should be able to handle all that throwing
+    @test m.isrunning(w)
+
+    m.stop(w)
+end
