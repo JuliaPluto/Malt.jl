f240cf17bb50f2df7d08daf0b1874661d37e151b
Fons van der Plas <fonsvdplas@gmail.com>
2023-01-17 17:24:44 +0100

    Add benchmark to CI

---
+++ test/benchmark.jl
@@ -0,0 +1,66 @@
+using BenchmarkTools
+using Test
+import Malt as m
+import Distributed
+
+# Set to true to fail tests if benchmark is too slow
+const TEST_BENCHMARK = false
+
+
+@testset "Benchmark" begin
+    
+    
+    w = m.Worker()
+    @test m.isrunning(w) === true
+    
+    
+    
+    p = Distributed.addprocs(1)[1]
+    
+    
+    
+    exprs = [
+        quote
+            sum(1:100) do i
+                sum(sin.(sqrt.(1:i))) / i
+            end
+        end
+    
+        quote
+            sleep(.01)
+        end
+    
+        quote
+            1+1
+        end
+    ]
+    
+    @testset "Expr $i" for i in eachindex(exprs)
+        ex = exprs[i]
+        
+        f1() = m.remote_eval_fetch(Main, w, ex)
+        f2() = Distributed.remotecall_eval(Main, p, ex)
+        
+        f1()
+        f2()
+        
+        @test f1() ≈ f2()
+        
+        t1 = @belapsed $f1()
+        t2 = @belapsed $f2()
+        
+        ratio = t1 / t2
+        
+        @info "Expr $i" ratio t1 t2 
+        
+        if TEST_BENCHMARK
+            @test (
+                # one of these should pass:
+                ratio < 1.2 || # < 20% slower, or
+                t1 - t2 < 0.002 # < 2ms slower
+            )
+        end
+    end
+    
+    m.stop(w)
+end
