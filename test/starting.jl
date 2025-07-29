41315805a5fc5d04f57317d467aacfcf7c2a8d69
Fons van der Plas <fonsvdplas@gmail.com>
2025-07-29 11:53:44 +0200

    Update starting.jl

--- test/starting.jl
+++ test/starting.jl
@@ -1,11 +1,12 @@
 
 
 @testset "Starting exceptions" begin
+    # Searching for strings requires Julia 1.8
+    needle = VERSION >= v"1.8.0" ? ["exited before we could connect", "threads"] : ErrorException
     
     tstart = time()
-    @test_throws ["exited before we could connect", "threads"] m.Worker(; exeflags = ["-t invalid"])
+    @test_throws needle m.Worker(; exeflags = ["-t invalid"])
     tend = time()
     
     @test tend - tstart < 15.0
-    
 end
