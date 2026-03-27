41dd2e685ea0a3758ef780485fd2bb84a4f1b9ff
Mosè Giordano <mose@gnu.org>
2026-03-27 02:48:22 +0000

    Fix tests in stable versions of Julia

--- test/starting.jl
+++ test/starting.jl
@@ -3,10 +3,14 @@
 @testset "Starting exceptions" begin
     # Searching for strings requires Julia 1.8
     needle = VERSION >= v"1.8.0" ? ["exited before we could connect", "threads"] : ErrorException
-    
+
     tstart = time()
     @test_throws needle m.Worker(; exeflags = ["-t invalid"])
     tend = time()
-    
-    @test tend - tstart < 15.0
+
+    # When we create a worker we start a timedwait (currently of 30 seconds), trying to
+    # connect to it.  When the Julia process is launched with invalid arguments, the poll
+    # will time out because it'll fail to connect to it.  Here we check that the whole
+    # process took a reasonable time around 30 seconds.
+    @test 25.0 < tend - tstart < 40.0
 end
